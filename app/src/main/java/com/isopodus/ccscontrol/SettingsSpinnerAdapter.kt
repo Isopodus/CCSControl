ed470b719e342af767431026159f7438ca840a30
Isopodus <isopodus@gmail.com>
2019-03-11 13:26:41 +0200

    -Fixed array adapter IndexOutOfBoundsExeption

--- app/src/main/java/com/isopodus/ccscontrol/SettingsSpinnerAdapter.kt
+++ app/src/main/java/com/isopodus/ccscontrol/SettingsSpinnerAdapter.kt
@@ -12,16 +12,21 @@ import android.widget.ArrayAdapter
 
 class SettingsSpinnerAdapter(context: Context, textViewResourceId: Int, objects: ArrayList<String>, private var colors: ArrayList<Int>) :
     ArrayAdapter<String>(context, textViewResourceId, objects) {
+
+    override fun getCount(): Int {
+        return colors.size
+    }
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val v = super.getView(position, convertView, parent)
-        if(colors.size != 0)
+        if(colors.size > position)
             (v as TextView).setTextColor(context!!.resources.getColorStateList(colors[position]))
         return v
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val v = super.getDropDownView(position, convertView, parent)
-        if(colors.size != 0)
+        if(colors.size > position)
             (v as TextView).setTextColor(context!!.resources.getColorStateList(colors[position]))
         return v
     }
